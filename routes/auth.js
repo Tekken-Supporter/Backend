d2b79e7407cc126661d811cd338e69495e6a397e
Sangwook Park <41150708+sw0501@users.noreply.github.com>
2023-09-26 02:22:47 +0900

    Feat: checkid API 추가
    중복된 id 체크 기능

--- routes/auth.js
+++ routes/auth.js
@@ -147,4 +147,56 @@ router.post('/register',async(req,res)=>{
     }
 });
 
+//회원가입
+router.post('/checkid/:id',async(req,res)=>{
+    res.header("Access-Control-Allow-Origin", "*");
+
+    try{
+        const connection = db.return_connection();
+
+        const id = req.params.id;
+
+        const SQL1 = "Select * from userinfo where id = ?;";
+
+        connection.query(SQL1,[id],function(err,results,field){
+            if(err){
+                console.error(err.toString());
+                return res.status(400).json({
+                    "type": "/errors/incorrect-SQL-pass",
+                    "title": "Incorrect query or SQL disconnect.",
+                    "status": 400,
+                    "detail": err.toString()
+                })
+            }
+
+            if(results[0] === undefined){
+                return res.status(200).json({
+                    "status": "ok",
+                    "message": "Not duplicate id.",
+                })
+            }
+            else {
+                return res.status(400).json({
+                    "type": "/errors/incorrect-auth-pass",
+                    "title": "Duplicate id",
+                    "status": 400,
+                    "detail": err.toString()
+                })
+            }
+            
+        })
+
+        
+    }
+    catch(err){
+        console.error(err.toString());
+        return res.status(500).json({
+            "type": "/errors/incorrect-server-pass",
+            "title": "Internal Server Error.",
+            "status": 500,
+            "detail": err.toString()
+        });
+    }
+});
+
 module.exports = router;
