0cdb880919d868c77af00a663135a1700b385671
Youngwoo Cho <cyw5066@naver.com>
2023-09-28 15:07:28 +0900

    add /character/info

--- routes/character.js
+++ routes/character.js
@@ -5,7 +5,16 @@ const db = require('../db.js');
 router.get('/main', (req, res) => {
     res.header("Access-Control-Allow-Origin", "*");
     const connection = db.return_connection();//DB 연결용 connection 변수 선언
-    connection.query('SELECT c_name, difficulty from champion', (error, rows) => {
+    connection.query('SELECT c_name, difficulty FROM champion', (error, rows) => {
+        if (error) throw error;
+        res.send(rows);
+    });
+});
+
+router.get('/info', function (req, res) {
+    res.header("Access-Control-Allow-Origin", "*");
+    const connection = db.return_connection();//DB 연결용 connection 변수 선언
+    connection.query(`SELECT * FROM champion WHERE c_name = '${req.query.c_name}'`, (error, rows) => {
         if (error) throw error;
         res.send(rows);
     });
