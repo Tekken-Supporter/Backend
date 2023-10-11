69eddce3614b53051ab673773c10565a1f7c94eb
Youngwoo Cho <cyw5066@naver.com>
2023-10-11 20:16:35 +0900

    add ranking.js

---
+++ routes/ranking.js
@@ -0,0 +1,23 @@
+const mysql = require('mysql');
+const router = require('express').Router();
+const db = require('../db.js');
+
+router.get('/', (req, res) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    const connection = db.return_connection();//DB 연결용 connection 변수 선언
+    connection.query('select group_concat(distinct name order by name), tier as tier from userinfo group by tier', (error, rows) => {
+        if (error) throw error;
+        res.json(rows);;
+    });
+});
+
+router.get('/rank', function (req, res) {
+    res.header("Access-Control-Allow-Origin", "*");
+    const connection = db.return_connection();//DB 연결용 connection 변수 선언
+    connection.query(`select name,id,champion,tier,winrate from userinfo order by tier limit 10`, (error, rows) => {
+        if (error) throw error;
+        res.json(rows);
+    });
+});
+
+module.exports = router;
